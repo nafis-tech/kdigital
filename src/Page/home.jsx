fd169b725de52213d1fbda4c77b1feea2bfa1745
nafis-tech <nafiezlela9@gmail.com>
2021-11-10 19:35:06 +0700

    judul

---
+++ src/Page/home.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import {
+    FormControl,
+    InputGroup,
+    Button,
+    Table,
+    Dropdown,
+    Card
+} from 'react-bootstrap'
+
+import { PHOTO, IMG_5, IMG_1, IMG_17 } from '../assets'
+
+
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            images: []
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Top Stories</h1>
+                <Card style={{ width: '20rem', marginLeft: '8vw', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)' }}>
+                    <Card.Img variant="top" src="https://media.istockphoto.com/photos/email-message-inbox-notification-on-laptop-screen-business-background-picture-id1229883762?b=1&k=20&m=1229883762&s=170667a&w=0&h=28x3ibf2iGnsC-07X_CIqKkrIIel4uuicWAzmQlN09Y=" />
+                    <Card.Body >
+                        <Card.Text style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', }}>
+                            <Button style={styles.btn} variant="warning" >Link Berita</Button>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+        )
+    }
+}
+
+const styles = {
+    btn: {
+        margin: '1vh',
+        width: '12vw',
+        marginLeft: '5vw',
+        marginBottom: '4vh',
+        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.12)',
+        borderRadius:'30px 30px'
+    },
+}
+
+export default Home
